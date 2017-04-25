f0614bc47ae0eaec1b66dc0dcc6d1a7604d92191
jemliF <jemlifathi2013@gmail.com>
2017-04-25 22:07:13 +0100

    initial commit

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function postQuote(Request $request)
+    {
+        $quote = new Quote();
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 201);
+    }
+
+    public function getQuotes(Request $request)
+    {
+        $quotes = Quote::all();
+        $response = [
+            'quotes' => $quotes
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function getQuote(Request $request, $id)
+    {
+
+    }
+
+    public function putQuote(Request $request, $id)
+    {
+        $quote = Quote::find($id);
+        if (!$quote) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 200);
+    }
+
+    public function deleteQuote($id)
+    {
+        $quote = Quote::find($id);
+        if (!$quote) {
+            return response()->json(['message' => 'Document not found'], 404);
+        } else {
+            $quote->delete();
+            return response()->json(['message' => 'Quote deleted'], 200);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/quote', [
+    'uses' => 'QuoteController@postQuote'
+]);
+
+Route::get('/quotes', [
+    'uses' => 'QuoteController@getQuotes'
+]);
+Route::get('/quote/{id}', [
+    'uses' => 'QuoteController@getQuote'
+]);
+
+Route::delete('/quote/{id}', [
+    'uses' => 'QuoteController@deleteQuote'
+]);
